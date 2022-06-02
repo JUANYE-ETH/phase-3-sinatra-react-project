259b961e2eea79c1da50f87e387f265e40a7a9c7
Vitalii <vetal.shtager@gmail.com>
2022-06-01 23:28:00 -0400

    clean and finished backend

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -11,19 +11,15 @@ class TodosController < ApplicationController
   end
 
   post '/todos' do
-    # create a new equipment in the database
-    # params is a hash of key-value pairs coming from the body of the request
     todo = Todo.create(
       text:         params[:text],
       category_id:  params[:category_id],
     )
     
-    # send back a response with the created equipment as JSON
     todo.to_json
   end
 
   patch '/todos/:id' do
-    # find the equipment using the ID
     todo = Todo.find(params[:id])
 
     todo.update(
@@ -31,19 +27,9 @@ class TodosController < ApplicationController
       category_id: params[:category_id]
     )
 
-    # send back the updated equipment as JSON
     todo.to_json
   end
 
-  # delete '/todos/:id' do
-  #   todo = Todo.find(params[:id].to_json)
-
-  #   todos_to_del = Todo.find(params[:id].to_json)
-
-  #   todos_to_del.destroy
-  #   todo.destroy
-  #   todo.to_json
-  # end
   delete '/todos/:id' do
     todo = Todo.find(params[:id])
     todo.destroy
